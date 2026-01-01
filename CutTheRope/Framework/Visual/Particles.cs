716c3aa3ddf585ebd01927007fc369db673bf443
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2026-01-01 12:18:16 +0700

    Remove no-op buffer operations from Particles base class

--- CutTheRope/Framework/Visual/Particles.cs
+++ CutTheRope/Framework/Visual/Particles.cs
@@ -90,11 +90,6 @@ namespace CutTheRope.Framework.Visual
             {
                 UpdateParticle(ref particles[particleIdx], delta);
             }
-            OpenGL.GlBindBuffer(2, verticesID);
-            OpenGL.GlBufferData(2, vertices, 3);
-            OpenGL.GlBindBuffer(2, colorsID);
-            OpenGL.GlBufferData(2, colors, 3);
-            OpenGL.GlBindBuffer(2, 0U);
         }
 
         protected override void Dispose(bool disposing)
@@ -104,16 +99,6 @@ namespace CutTheRope.Framework.Visual
                 particles = null;
                 vertices = null;
                 colors = null;
-                if (verticesID != 0)
-                {
-                    OpenGL.GlDeleteBuffers(1, ref verticesID);
-                    verticesID = 0;
-                }
-                if (colorsID != 0)
-                {
-                    OpenGL.GlDeleteBuffers(1, ref colorsID);
-                    colorsID = 0;
-                }
                 texture = null;
             }
             base.Dispose(disposing);
@@ -142,8 +127,6 @@ namespace CutTheRope.Framework.Visual
             }
             active = false;
             blendAdditive = false;
-            OpenGL.GlGenBuffers(1, ref verticesID);
-            OpenGL.GlGenBuffers(1, ref colorsID);
             return this;
         }
 
@@ -286,8 +269,6 @@ namespace CutTheRope.Framework.Visual
         public PointSprite[] vertices;
 
         public RGBAColor[] colors;
-
-        private uint verticesID;
 
         public uint colorsID;
 
